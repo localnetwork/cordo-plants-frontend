8a800f744265650813f93d561c6c664875e3b8e2
Dion <diome.halcyonwebdesign@gmail.com>
2023-09-24 18:30:17 +0800

    fix: meta

--- components/Meta/Meta.js
+++ components/Meta/Meta.js
@@ -1,6 +1,13 @@
-import React from "react";
 import Head from "next/head";
+import React, { useState, useEffect } from "react";
 export const Meta = () => {
+  const [currentDomain, setCurrentDomain] = useState("");
+  useEffect(() => {
+    const domain = window.location.protocol + "//" + window.location.host;
+
+    setCurrentDomain(domain);
+  }, []);
+
   return (
     <>
       <Head>
@@ -9,7 +16,10 @@ export const Meta = () => {
           name="description"
           content={`Cordoplants is a web-app community based application where we connect partners/organizations to it's community. We are empowering partners to contribute to tree-planting initiatives. Through their support, volunteers can accumulate "Cordo-points" that can be converted into real-money. The goal for this app is to ease the immense global warming.`}
         />
-        <meta name="" href="/images/opengraph/og-default.jpg" />
+        <meta
+          property="og:image"
+          content={`${currentDomain}/images/opengraph/og-default.jpg`}
+        />
       </Head>
     </>
   );
